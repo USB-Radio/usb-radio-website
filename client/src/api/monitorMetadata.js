4db524d24650972b53904c9c698dbc4ceb3c97c8
Manuel Sepulveda <mssepulveda.87@gmail.com>
2024-11-07 23:15:00 -0500

    actualizando nuevamente monitorMetadata.js

--- client/src/api/monitorMetadata.js
+++ client/src/api/monitorMetadata.js
@@ -1,9 +1,6 @@
 import axios from "axios";
 
-const API_data =
-  window.location.hostname === "localhost"
-    ? `http://localhost:3000/api`
-    : `http://64.227.11.166:3000/api`;
+const API_data = "http://64.227.11.166:3000/api";
 
 export const monitorDataRequest = () => axios.get(`${API_data}/data`);
 export const azuraDataRequest = () => axios.get(`${API_data}/radio`);
