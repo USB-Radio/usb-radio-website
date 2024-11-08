558dedab741c33b41e2c7a316c924197864ce088
Manuel Sepulveda <mssepulveda.87@gmail.com>
2024-11-07 22:54:25 -0500

    modificado monitorMetadata.js para tener una URL dinamica

--- client/src/api/monitorMetadata.js
+++ client/src/api/monitorMetadata.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
-const API = "http://64.227.11.166:3000/api";
+const API =
+  window.location.hostname === "localhost"
+    ? `http://localhost:3000/api`
+    : `http://64.227.11.166:3000/api`;
 
 export const monitorDataRequest = () => axios.get(`${API}/data`);
 export const azuraDataRequest = () => axios.get(`${API}/radio`);
