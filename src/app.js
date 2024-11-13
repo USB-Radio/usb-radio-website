3470908635962139f11b21fb7a5d70757e893141
Manuel Sepulveda <mssepulveda.87@gmail.com>
2024-11-13 12:09:23 -0500

    actualizacion 4

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(
   cors({
     // origin: "http://localhost:5173",
-    origin: "http://radiousbbog.tech:5173",
+    origin: "http://radiousbbog.tech",
   })
 );
 app.use(morgan("dev"));
