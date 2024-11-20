902dd2e797725d17cbf837f0478f1e5284965276
Andres Sierra <102034926+AndreSierraM@users.noreply.github.com>
2024-11-20 11:34:22 -0500

    se actualiza la URL de escucha en el componente AudioPlayer

--- client/src/components/AudioPlayer.jsx
+++ client/src/components/AudioPlayer.jsx
@@ -15,7 +15,7 @@ function AudioPlayer() {
   const { radioData } = useData();
   const audioRef = useRef(null);
   const {
-    listenUrl = "https://a6.asurahosting.com:7340/radio.mp3",
+    listenUrl = "https://a6.asurahosting.com/api/nowplaying/usb_radio",
     totalListeners = 0,
     currentPlaying: {
       duration = 0,
